bb9ced385d200ac81e182cc132b2237063fe79a5
Igor <igorsvk16@gmail.com>
2026-05-23 19:43:20 +0300

    add http request for add task

--- vite-project/src/api/http.js
+++ vite-project/src/api/http.js
@@ -1,3 +1,10 @@
 export async function addTask(taskText) {
-    const response = await fetch()
+    const response = await fetch("https://easydev.club/api/v1/todos", {
+        method: 'POST',
+        body: JSON.stringify({taskText}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+)
 }
