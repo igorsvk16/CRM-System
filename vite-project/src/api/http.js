c1fc243bef2722037ddc05b85d37d6e456a83f6c
Igor <igorsvk16@gmail.com>
2026-05-26 21:53:07 +0300

    fix: body in request shouldnt be object in object

--- vite-project/src/api/http.js
+++ vite-project/src/api/http.js
@@ -2,8 +2,8 @@ export async function fetchAddTask(title, isDone) {
     const response = await fetch("https://easydev.club/api/v1/todos", {
         method: 'POST',
         body: JSON.stringify({
-            title: {title},
-            isDone: {isDone} }),
+            title: title,
+            isDone: isDone }),
         headers: {
             'Content-Type': 'application/json'
         }
