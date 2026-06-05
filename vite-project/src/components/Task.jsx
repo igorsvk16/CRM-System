4a2b2e2199f42e21d1a4a5474623ed1b0f9a3709
Igor <igorsvk16@gmail.com>
2026-06-05 12:19:37 +0300

    add input for edit task

--- vite-project/src/components/Task.jsx
+++ vite-project/src/components/Task.jsx
@@ -1,12 +1,21 @@
 import editIcon from '../assets/edit.png'
 import trashIcon from '../assets/trash-bin.png'
 import {fetchTaskIsDone} from "../api/http.js";
-import { useEffect, useRef } from "react";
+import {useEffect, useRef, useState} from "react";
 // import './Task.css'
 
 
 export default function Task({ tasks, isLoading, loadingText }) {
-    let editable = true;
+
+    const [ isEditMode, setIsEditMode ] = useState(false);
+    const inputRef = useRef(null);
+    function turnOnEditMode() {
+        setIsEditMode(true);
+        inputRef.current.focus();
+    }
+
+
+    // let editable = true;
 
     async function onSelectStatus(taskData) {
         let newStatus = taskData.isDone;
@@ -38,32 +47,45 @@ export default function Task({ tasks, isLoading, loadingText }) {
             {isLoading && <p className="fallback-text">{loadingText}</p>}
             {!isLoading && tasks.length === 0 && <p className="fallback-text">Add your first task</p>}
             {!isLoading && tasks.length > 0 && (
-                <dialog className="tasks">
+                <section className="tasks">
                     {(tasks).map((taskData) => (
                         <li key={taskData.id} className="task">
-                            <input type={"checkbox"} onClick={() => onSelectStatus(taskData)} />
-                            {!editable &&
-                                <>
-                                    <p>{taskData.title}</p>
-                                    <button onClick={() => onSelectEdit()}></button>
-                                    <button>
-                                        <img className="actionIcon" src={editIcon} />
-                                    </button>
-                                    <button onClick={() => onSelectDelete()}>
-                                     <img className="actionIcon" src={trashIcon} />
-                                    </button>
-                                </>
-                            }
-                            {editable && <input
-                                placeholder={taskData.title}
-
+                            <input
+                            ref={inputRef}
+                            type={"text"}
+                            // autoFocus
+                            value={taskData.title}
+                            readOnly={!isEditMode}
+                            onClick={turnOnEditMode}
+                            onBlur={() => setIsEditMode(false)}
                             />
 
-                            }
+
+
+                            {/*<input type={"checkbox"} onClick={() => onSelectStatus(taskData)} />*/}
+                            {/*{!editable &&*/}
+                            {/*    <>*/}
+                            {/*        */}
+                            {/*        <p>{taskData.title}</p>*/}
+                            {/*        <button onClick={() => onSelectEdit()}></button>*/}
+                            {/*        <button>*/}
+                            {/*            <img className="actionIcon" src={editIcon} />*/}
+                            {/*        </button>*/}
+                            {/*        <button onClick={() => onSelectDelete()}>*/}
+                            {/*         <img className="actionIcon" src={trashIcon} />*/}
+                            {/*        </button>*/}
+                            {/*    </>*/}
+                            {/*}*/}
+                            {/*{editable && <input*/}
+                            {/*    placeholder={taskData.title}*/}
+
+                            {/*/>*/}
+
+                            {/*}*/}
 
                         </li>
                     ))}
-                </dialog>
+                </section>
             )}
         </section>
     )
