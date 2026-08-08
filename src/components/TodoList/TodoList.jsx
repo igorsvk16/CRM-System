e38572789d6a68ca56fd010f643d48dbff56c403
Igor <igorsvk16@gmail.com>
2026-08-08 14:24:20 +0300

    fix visible background tasks color on deploy

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,7 +3,7 @@ import styles from "./TodoList.module.css"
 
 export default function TodoList({ todos, updateTodos, currentCategory, isLoading, checkValidation }) {
     return (
-            <section>
+            <section className={styles.todoListContainer}>
                 {isLoading && <p className={styles.centeredText}>Загрузка задач...</p>}
                 {!isLoading && todos.length === 0 && <p className={styles.centeredText}>Добавьте свою первую задачу</p>}
                 {!isLoading && todos.length > 0 && (
