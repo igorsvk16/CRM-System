440f4ac8b6fc164a88d88ea02bc365c3388506dd
Igor <igorsvk16@gmail.com>
2026-08-09 14:02:33 +0300

    refactor: move todo page to component from App.jsx

---
+++ src/page/Todo.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState} from "react";
+import { getTodos } from "../api/http.js";
+import TodoList from "../components/TodoList/TodoList.jsx";
+import TodoAdd from "../components/TodoAdd/TodoAdd.jsx";
+import TodosFilter from "../components/TodosFilter/TodosFilter.jsx";
+import { MIN_LENGTH, MAX_LENGTH } from "../constants.jsx";
+
+export default function Todo() {
+    const [ todos, setTodos ] = useState([]);
+    const [ todoCounter, setTodoCounter ] = useState([]);
+    const [ currentCategory, setCurrentCategory ] = useState('all');
+    const [ isLoading, setIsLoading ] = useState(true);
+
+    useEffect(() => {
+        function fetchTodos() {
+            updateTodos(currentCategory).then(() =>
+                setIsLoading(false));
+        }
+        fetchTodos();
+    }, [currentCategory]);
+
+    async function updateTodos(currentCategory) {
+        try {
+            const todos = await getTodos(currentCategory);
+            setTodos(todos.data);
+            setTodoCounter(todos.info)
+        } catch (error) {
+            alert('Ошибка при обновлении задач');
+            alert(error);
+        }
+    }
+
+    function checkValidation(todoInput, setTodoInput) {
+        if (todoInput.trim().length > MAX_LENGTH) {
+            alert("Максимальная длина текста 64 символа");
+            return false;
+        } else if (todoInput.trim().length === MIN_LENGTH - 1) {
+            alert("Минимальная длина текста 2 символа");
+            return false;
+        } else if (todoInput.trim().length === 0) {
+            setTodoInput('');
+            alert("Введите текст, не пробелы");
+            return false;
+        } else {
+            return true;
+        }
+    }
+    return (
+        <main>
+            <TodoAdd
+                updateTodos={updateTodos}
+                currentCategory={currentCategory}
+                checkValidation={checkValidation}
+            />
+            <TodosFilter
+                currentCategory={currentCategory}
+                todoCounter={todoCounter}
+                setCurrentCategory={setCurrentCategory}
+                updateTodos={updateTodos}
+            />
+            <TodoList
+                todos={todos}
+                updateTodos={updateTodos}
+                currentCategory={currentCategory}
+                isLoading={isLoading}
+                checkValidation={checkValidation}
+            />
+        </main>
+    )
+
+}
