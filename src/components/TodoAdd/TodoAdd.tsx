456c8da4bbea6f550f7524df19df298aa64425f0
Igor <igorsvk16@gmail.com>
2026-08-18 23:59:04 +0300

    refactor: rewrite TodoAdd on TS

--- src/components/TodoAdd/TodoAdd.tsx
+++ src/components/TodoAdd/TodoAdd.tsx
@@ -1,22 +1,24 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import {addTodo} from "../../api/http.js";
 import checkTitleValidation from "../../utils/helpers/checkTitleValidation.jsx";
 import styles from "./TodoAdd.module.css";
 
-const TodoAdd: React.FC = ({ updateTodos: () = > void, currentCategory: string }) =>  {
+const TodoAdd: React.FC<({ updateTodos: (text: string) => void, currentCategory: string })> = (props) =>  {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     let isDone= false;
 
     const [ todoInput, setTodoInput ] = useState<string>("");
 
-    const fetchAddTodo() {
-        const validateTitle = checkTitleValidation(todoInput);
+    const fetchAddTodo(e: React.FormEvent) => {
+        // const enteredText = todoTextInputRef.current!.value;
+        const validateTitle = checkTitleValidation(enteredText);
         if (validateTitle) {
             alert(validateTitle);
         } else {
             addTodo(todoInput, isDone)
                 .then(() => {
-                    updateTodos(currentCategory);
+                    props.updateTodos(props.currentCategory);
                     setTodoInput('');
                 }, reason => {
                     alert("Ошибка при добавлении задачи");
@@ -25,6 +27,30 @@ const TodoAdd: React.FC = ({ updateTodos: () = > void, currentCategory: string }
             )
         }
     }
+    return (
+        <form onSubmit={fetchAddTodo} className={styles.todoAdd}>
+            <label htmlFor="text">Текст задачи</label>
+            <input
+                type="text"
+                id="text"
+                value={todoInput}
+                // ref={todoTextInputRef}
+                onChange={e => {
+                    setTodoInput(e.target.value);
+                }}
+                placeholder="Новая задача..."
+                className={styles.inputNewTodo}
+            />
+            <button
+                className={styles.addButton}
+            >
+                Добавить
+            </button>
+        </form>
+    );
+};
+
+export default TodoAdd;
 
     // let isDone= false;
     //
@@ -47,23 +73,3 @@ const TodoAdd: React.FC = ({ updateTodos: () = > void, currentCategory: string }
     //     }
     // }
 
-    return (
-        <form className={styles.todoAdd} action={fetchAddTodo}>
-            <input
-                className={styles.inputNewTodo}
-                value={todoInput}
-                onChange={e => {
-                    setTodoInput(e.target.value);
-                }}
-                type="text"
-                placeholder="Новая задача..."
-            />
-            <button
-                type="submit"
-                className={styles.addButton}
-            >
-                Добавить
-            </button>
-        </form>
-    )
-}
