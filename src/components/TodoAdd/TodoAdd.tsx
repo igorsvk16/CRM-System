a5dcc71e9c1b22e18524ac65b86554ecb4a69318
Igor <igorsvk16@gmail.com>
2026-08-19 01:49:12 +0300

    refactor: finish rewriting TodoAdd component on TS

--- src/components/TodoAdd/TodoAdd.tsx
+++ src/components/TodoAdd/TodoAdd.tsx
@@ -1,41 +1,40 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, SetStateAction} from "react";
 import {addTodo} from "../../api/http.js";
 import checkTitleValidation from "../../utils/helpers/checkTitleValidation.jsx";
 import styles from "./TodoAdd.module.css";
 
-const TodoAdd: React.FC<({ updateTodos: (text: string) => void, currentCategory: string })> = (props) =>  {
-    const todoTextInputRef = useRef<HTMLInputElement>(null);
+const TodoAdd: React.FC<({ updateTodos: (text: string) => void, currentCategory: string })> = (props) => {
+    // const todoTextInputRef = useRef<HTMLInputElement>(null);
 
-    let isDone= false;
+    let isDone = false;
 
-    const [ todoInput, setTodoInput ] = useState<string>("");
+    const [todoInput, setTodoInput] = useState<string>("");
 
-    const fetchAddTodo(e: React.FormEvent) => {
-        // const enteredText = todoTextInputRef.current!.value;
-        const validateTitle = checkTitleValidation(enteredText);
+
+    const fetchAddTodo = (event: React.FormEvent) => {
+        const validateTitle = checkTitleValidation(todoInput);
         if (validateTitle) {
             alert(validateTitle);
         } else {
+            console.log(todoInput);
             addTodo(todoInput, isDone)
                 .then(() => {
-                    props.updateTodos(props.currentCategory);
-                    setTodoInput('');
-                }, reason => {
-                    alert("Ошибка при добавлении задачи");
-                    alert(reason);
-                }
-            )
+                        props.updateTodos(props.currentCategory);
+                        setTodoInput('');
+                    }, reason => {
+                        alert("Ошибка при добавлении задачи");
+                        alert(reason);
+                    }
+                )
         }
     }
     return (
         <form onSubmit={fetchAddTodo} className={styles.todoAdd}>
-            <label htmlFor="text">Текст задачи</label>
             <input
                 type="text"
                 id="text"
                 value={todoInput}
-                // ref={todoTextInputRef}
-                onChange={e => {
+                onChange={(e: { target: { value: SetStateAction<string>; }; }) => {
                     setTodoInput(e.target.value);
                 }}
                 placeholder="Новая задача..."
@@ -51,25 +50,4 @@ const TodoAdd: React.FC<({ updateTodos: (text: string) => void, currentCategory:
 };
 
 export default TodoAdd;
-
-    // let isDone= false;
-    //
-    // const [ todoInput, setTodoInput ] = useState("");
-    //
-    // function fetchAddTodo() {
-    //     const validateTitle = checkTitleValidation(todoInput);
-    //     if (validateTitle) {
-    //         alert(validateTitle);
-    //     } else {
-    //         addTodo(todoInput, isDone)
-    //             .then(() => {
-    //                     updateTodos(currentCategory);
-    //                     setTodoInput('');
-    //                 }, reason => {
-    //                     alert("Ошибка при добавлении задачи");
-    //                     alert(reason);
-    //                 }
-    //             )
-    //     }
-    // }
 
