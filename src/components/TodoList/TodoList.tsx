c7fcad738a19e746fe8e6ea6159793df1e45f582
Igor <igorsvk16@gmail.com>
2026-08-20 19:38:22 +0300

    fix: change vite project type from jsx to txs

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,19 +1,20 @@
-import TodoItem from '../TodoItem/TodoItem.tsx';
+import TodoItem from '../TodoItem/TodoItem';
 import styles from "./TodoList.module.css"
+import React from "react";
 
-export default function TodoList({ todos, updateTodos, currentCategory, isLoading }) {
+const TodoList: React.FC<{ todos: any, updateTodos: (text: string) => void, currentCategory: string, isLoading: boolean }> = (props) => {
     return (
             <section className={styles.todoListContainer}>
-                {isLoading && <p className={styles.centeredText}>Загрузка задач...</p>}
-                {!isLoading && todos.length === 0 && <p className={styles.centeredText}>Добавьте свою первую задачу</p>}
-                {!isLoading && todos.length > 0 && (
+                {props.isLoading && <p className={styles.centeredText}>Загрузка задач...</p>}
+                {!props.isLoading && props.todos.length === 0 && <p className={styles.centeredText}>Добавьте свою первую задачу</p>}
+                {!props.isLoading && props.todos.length > 0 && (
                     <section>
-                        {(todos).map((todoData) => (
+                        {(props.todos).map((todoData: { id: any; title: string; isDone: boolean; }) => (
                             <TodoItem
                                 key={todoData.id}
                                 todoData={todoData}
-                                updateTodos={updateTodos}
-                                currentCategory={currentCategory}
+                                updateTodos={props.updateTodos}
+                                currentCategory={props.currentCategory}
                             />
                         ))}
                     </section>
@@ -21,3 +22,26 @@ export default function TodoList({ todos, updateTodos, currentCategory, isLoadin
             </section>
     )
 }
+
+export default TodoList;
+
+// export default function TodoList({ todos, updateTodos, currentCategory, isLoading }) {
+//     return (
+//         <section className={styles.todoListContainer}>
+//             {isLoading && <p className={styles.centeredText}>Загрузка задач...</p>}
+//             {!isLoading && todos.length === 0 && <p className={styles.centeredText}>Добавьте свою первую задачу</p>}
+//             {!isLoading && todos.length > 0 && (
+//                 <section>
+//                     {(todos).map((todoData) => (
+//                         <TodoItem
+//                             key={todoData.id}
+//                             todoData={todoData}
+//                             updateTodos={updateTodos}
+//                             currentCategory={currentCategory}
+//                         />
+//                     ))}
+//                 </section>
+//             )}
+//         </section>
+//     )
+// }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,1 @@
-// Source - https://stackoverflow.com/a/75192263
-// Posted by NetYogi
-// Retrieved 2026-08-20, License - CC BY-SA 4.0
-
 /// <reference types="vite/client" />
