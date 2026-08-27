b860d006892d62adb40635e35e544f08df6d1a72
Igor <igorsvk16@gmail.com>
2026-08-28 00:04:19 +0300

    refactor: delete unused component

--- src/components/TodosFilter/TodosFilter.tsx
+++ src/components/TodosFilter/TodosFilter.tsx
@@ -1,6 +1,5 @@
 import styles from './TodosFilter.module.css';
 import React from "react";
-import TodoCategory from "../../ui/TodoCategory/TodoCategory.tsx";
 
 const TodosFilter: React.FC<{currentCategory: string, todoCounter: {todo: number, inProgress: number, done: number}, setCurrentCategory: (newState: string) => void, updateTodos: (text: string) => void}> = (props) => {
 
@@ -10,20 +9,16 @@ const TodosFilter: React.FC<{currentCategory: string, todoCounter: {todo: number
     }
     return (
         <div className={styles.todosCategories}>
-            <TodoCategory
-            currentCategory={props.currentCategory}
-            handleChangeCategory={handleChangeCategory}
-            category="todo"
-            >
+            <button
+                className={props.currentCategory === 'todo' ? styles.todoCategoryActive : styles.todoCategory}
+                onClick={() => handleChangeCategory('todo')}>
                 В работе ({props.todoCounter.todo})
-            </TodoCategory>
-            <TodoCategory
-                currentCategory={props.currentCategory}
-                handleChangeCategory={handleChangeCategory}
-                category="done"
-            >
+            </button>
+            <button
+                className={props.currentCategory === 'done' ? styles.todoCategoryActive : styles.todoCategory}
+                onClick={() => handleChangeCategory('done')}>
                 Сделано ({props.todoCounter.done})
-            </TodoCategory>
+            </button>
         </div>
     )
 }
