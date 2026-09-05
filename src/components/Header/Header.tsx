9751299edc1098956f20592727b2a9e36f8ce026
Igor <igorsvk16@gmail.com>
2026-09-06 00:35:08 +0300

    feat: start adding new Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <>
+            <h1>
+                Pulse CRM
+            </h1>
+                <div>
+                    <p>Рабочий стол</p>
+                    <p>Задачи</p>
+                    <p>Команда</p>
+                </div>
+            <button>Создать</button>
+        </>
+    )
+}
+
+export default Header;
--- src/components/TodosFilter/TodosFilter.tsx
+++ src/components/TodosFilter/TodosFilter.tsx
@@ -5,15 +5,11 @@ import {TaskData, TasksData} from "../../type/interface/TasksData.ts";
 
 const TodosFilter: React.FC<{currentCategory: string, todoCounter: {todo: number, inProgress: number, review: number, readyForRelease: number, onHold: number,  done: number}, setCurrentCategory: (newState: string) => void, updateTodos: (text: string) => void, todos: TasksData, isLoading: boolean}> = (props) => {
 
-    console.log("props.todos");
-    // console.log(props.todos);
-    // console.log(props.todos.filter(todo => todo.status === "todo"));
-
-
     function handleChangeCategory(categoryName: string) {
         props.setCurrentCategory(categoryName);
         props.updateTodos(categoryName);
     }
+
     return (
         <>
         <div className={styles.todosCategories}>
@@ -40,7 +36,6 @@ const TodosFilter: React.FC<{currentCategory: string, todoCounter: {todo: number
                     </section>
                 )}
             </div>
-
             <div>
             <button
                 className={props.currentCategory === 'inProgress' ? styles.todoCategoryActive : styles.todoCategory}
@@ -70,11 +65,22 @@ const TodosFilter: React.FC<{currentCategory: string, todoCounter: {todo: number
                 onClick={() => handleChangeCategory('review')}>
                 Ревью ({props.todoCounter.review})
             </button>
-                <div className={styles.todoContainer}>
-                    <p>tsertet</p>
-                    <p>tsertet</p>
-                    <p>tsertet</p>
-                </div>
+                {props.isLoading && <p className={styles.centeredText}>Загрузка задач...</p>}
+                {!props.isLoading && props.todos.length === 0 && <p className={styles.centeredText}>Добавьте свою первую задачу</p>}
+                {!props.isLoading && props.todos.length > 0 && (
+                    <section>
+                        {(props.todos)
+                            .filter(todoData => todoData.status === "review")
+                            .map((todoData: TaskData) => (
+                                <TodoItem
+                                    key={todoData.id}
+                                    todoData={todoData}
+                                    updateTodos={props.updateTodos}
+                                    currentCategory={props.currentCategory}
+                                />
+                            ))}
+                    </section>
+                )}
             </div>
             <div>
             <button
@@ -82,11 +88,22 @@ const TodosFilter: React.FC<{currentCategory: string, todoCounter: {todo: number
                 onClick={() => handleChangeCategory('readyForRelease')}>
                 Готово к релизу ({props.todoCounter.readyForRelease})
             </button>
-                <div className={styles.todoContainer}>
-                    <p>tsertet</p>
-                    <p>tsertet</p>
-                    <p>tsertet</p>
-                </div>
+                {props.isLoading && <p className={styles.centeredText}>Загрузка задач...</p>}
+                {!props.isLoading && props.todos.length === 0 && <p className={styles.centeredText}>Добавьте свою первую задачу</p>}
+                {!props.isLoading && props.todos.length > 0 && (
+                    <section>
+                        {(props.todos)
+                            .filter(todoData => todoData.status === "readyForRelease")
+                            .map((todoData: TaskData) => (
+                                <TodoItem
+                                    key={todoData.id}
+                                    todoData={todoData}
+                                    updateTodos={props.updateTodos}
+                                    currentCategory={props.currentCategory}
+                                />
+                            ))}
+                    </section>
+                )}
             </div>
             <div>
             <button
@@ -94,11 +111,22 @@ const TodosFilter: React.FC<{currentCategory: string, todoCounter: {todo: number
                 onClick={() => handleChangeCategory('onHold')}>
                 На паузе ({props.todoCounter.onHold})
             </button>
-                <div className={styles.todoContainer}>
-                    <p>tsertet</p>
-                    <p>tsertet</p>
-                    <p>tsertet</p>
-                </div>
+                {props.isLoading && <p className={styles.centeredText}>Загрузка задач...</p>}
+                {!props.isLoading && props.todos.length === 0 && <p className={styles.centeredText}>Добавьте свою первую задачу</p>}
+                {!props.isLoading && props.todos.length > 0 && (
+                    <section>
+                        {(props.todos)
+                            .filter(todoData => todoData.status === "onHold")
+                            .map((todoData: TaskData) => (
+                                <TodoItem
+                                    key={todoData.id}
+                                    todoData={todoData}
+                                    updateTodos={props.updateTodos}
+                                    currentCategory={props.currentCategory}
+                                />
+                            ))}
+                    </section>
+                )}
             </div>
             <div>
             <button
@@ -106,21 +134,24 @@ const TodosFilter: React.FC<{currentCategory: string, todoCounter: {todo: number
                 onClick={() => handleChangeCategory('done')}>
                 Выполнено ({props.todoCounter.done})
             </button>
-                <div className={styles.todoContainer}>
-                    <p>tsertet</p>
-                    <p>tsertet</p>
-                    <p>tsertet</p>
-                </div>
+                {props.isLoading && <p className={styles.centeredText}>Загрузка задач...</p>}
+                {!props.isLoading && props.todos.length === 0 && <p className={styles.centeredText}>Добавьте свою первую задачу</p>}
+                {!props.isLoading && props.todos.length > 0 && (
+                    <section>
+                        {(props.todos)
+                            .filter(todoData => todoData.status === "done")
+                            .map((todoData: TaskData) => (
+                                <TodoItem
+                                    key={todoData.id}
+                                    todoData={todoData}
+                                    updateTodos={props.updateTodos}
+                                    currentCategory={props.currentCategory}
+                                />
+                            ))}
+                    </section>
+                )}
             </div>
         </div>
-        {/*<div className={styles.todoColumn}>*/}
-        {/*    <div className={styles.todoContainer}>*/}
-        {/*        <p>sadas</p>*/}
-        {/*        <p>sadas</p>*/}
-        {/*        <p>sadas</p>*/}
-        {/*    </div>*/}
-        {/*</div>*/}
-
         </>
     )
 }
