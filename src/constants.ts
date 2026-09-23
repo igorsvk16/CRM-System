bdbbd834800a2d6a5906d7d2efeff70ff6bd309a
Igor <igorsvk16@gmail.com>
2026-09-23 18:50:04 +0300

    feat: update new length validation

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const MIN_LENGTH = 2;
+export const MIN_LENGTH = 4;
 export const MAX_LENGTH = 64;
--- src/utils/helpers/checkTitleValidation.ts
+++ src/utils/helpers/checkTitleValidation.ts
@@ -6,7 +6,7 @@ export default function checkTitleValidation(todoInput: string): string {
     if (inputTrimmedLength > MAX_LENGTH) {
         return "Максимальная длина текста 64 символа";
     } else if (inputTrimmedLength === MIN_LENGTH - 1) {
-        return "Минимальная длина текста 2 символа";
+        return "Минимальная длина текста 4 символа";
     } else if (inputTrimmedLength === 0) {
         return "Введите текст, не пробелы";
     }
